d598985d0bab6cb44b7807927e29fbfa50653581
Administrator <Administrator@WIN712340930VFG>
2020-04-28 03:13:07 +0530

    first commit

---
+++ AmazonS3Connectivity/src/main/java/com/aws/s3/constants/CommanConstant.java
@@ -0,0 +1,28 @@
+package com.aws.s3.constants;
+
+public class CommanConstant {
+
+	
+	public static String ACCESS_KEY_ID = "AKIAUAUIEGPTT2OHZRPO";
+	
+	public static String ACCESS_SEC_KEY = "D8SPGC0gF1+NDl2tcIU3MnuexZaSlOBt9EzxjR32";
+	
+	public static String BUCKET_NAME = "hibucket12";
+	
+	public static String FOLDER_NAME = "HiNitin12";
+	
+	public static String FILE_NAME = "SHEKHAR-SIR-SPRING-.pdf";
+	
+	public static String FILE_PATH = "C:\\\\Users\\\\Administrator\\\\Desktop\\\\SHEKHAR-SIR-SPRING-.pdf";
+	
+	public static String BUCKET_FILE_PATH = "HiNitin12/SHEKHAR-SIR-SPRING-.pdf";
+	
+	public static String LOCAL_DOWNLOAD_PATH = "C:\\\\Users\\\\Administrator\\\\Desktop\\\\SHEKHAR-SIR-SPRING-.pdf";
+	
+	public static final String SUFFIX = "/";
+	
+	public static String STORE_PATH = "";
+
+	
+}
+	
